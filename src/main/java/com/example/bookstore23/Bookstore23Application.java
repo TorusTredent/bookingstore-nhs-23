39c6e17bfc9ca7a65b775d2134b1260812d603b9
TorusTredent <torus.tredent@gmail.com>
2023-03-10 18:12:36 +0300

    init

---
+++ src/main/java/com/example/bookstore23/Bookstore23Application.java
@@ -0,0 +1,13 @@
+package com.example.bookstore23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bookstore23Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bookstore23Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookstore23/Bookstore23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookstore23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bookstore23ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
