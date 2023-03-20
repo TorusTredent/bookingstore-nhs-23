c514035131d31fc180b280bd61133e69d0acc4cb
Andrey <xrapa07@mail.ru>
2023-03-20 22:12:20 +0300

    add  user profile

---
+++ src/main/java/com/example/bookstore23/services/UserService.java
@@ -0,0 +1,14 @@
+package com.example.bookstore23.services;
+
+import com.example.bookstore23.dto.ProfileDTO;
+import com.example.bookstore23.entity.User;
+
+public interface UserService {
+    User getByUserName(String userName);
+
+    ProfileDTO mapUserToProfileDto(User user1);
+
+   void mapProfileDTOToUser(User user1, ProfileDTO profileDTO);
+
+    User save(User user1);
+}
